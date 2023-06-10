617e11bbbd52d42932b712c5e9764fd58261515e
Dev-Bastians <workingkeo@gmail.com>
2023-06-10 15:37:40 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const { Client, Collection, GatewayIntentBits, Events } =  require("discord.js");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms)); // delay time
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // discord.js handler
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+client.on("ready", () => {
+  console.log("Bot Ready!");
+});
